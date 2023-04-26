9a0db3ab8c19d65d88ee86f9a78c271deb44f9e0
Liubomyr Pavlykivskyi <65455052+L0PE@users.noreply.github.com>
2023-04-26 21:45:31 +0300

    Created health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn heals_check(request: HttpRequest) -> impl Responder {
+    let name = request.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(heals_check))
+            .route("/{name}", web::get().to(heals_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
