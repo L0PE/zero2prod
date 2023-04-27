8a10c3bd907528e788b71aa7d40a2fcd8b335965
Liubomyr Pavlykivskyi <65455052+L0PE@users.noreply.github.com>
2023-04-27 20:55:51 +0300

    Added tests for health endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind a random port");
+
+    run(listener)?.await
 }
