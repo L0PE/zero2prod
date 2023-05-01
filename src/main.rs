6c239403e183d8f075a3162fc1748e3042eaba8c
Liubomyr Pavlykivskyi <65455052+L0PE@users.noreply.github.com>
2023-05-01 14:40:06 +0300

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::Settings;
@@ -9,6 +10,8 @@ async fn main() -> std::io::Result<()> {
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind a random port");
+
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let database_pool = PgPool::connect(&configuration.database_settings.get_connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -26,9 +26,12 @@ pub async fn subscribe(
     .execute(connection.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved!");
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query {}!", e);
+            log::error!("Failed to execute query {:?}!", e);
             HttpResponse::InternalServerError().finish()
         }
     }
